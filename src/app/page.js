61df050ec52d86f0c13f8b7dbe8597ba0ba212d6
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-07-17 13:06:48 +0600

    add: footer component add

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="flex justify-center items-center text-3xl font-bold underline">Welcome to Next.js!</h1>
+      <h1 className="flex justify-center items-center text-3xl font-bold underline h-screen">Welcome to Next.js!</h1>
     </div>
   );
 }
